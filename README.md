b3fed3c880aacb1f57ab67dad814bce9b6fb2044
Taron Foxworth <taronfoxworth@gmail.com>
2017-08-25 14:15:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Losant Raspberry PI Python Example
+
+Example Python application that connects a Raspberry PI to the [Losant IoT developer](https://www.losant.com) platform. Visit the [Losant documentation](https://docs.losant.com/getting-started/boards/getting-started-with-raspberry-pi) for further details.
+
+## Dependencies
+
+```
+$ sudo pip install losant-mqtt gpiozero
+``` 
---
+++ index.py
@@ -0,0 +1,34 @@
+import json
+from gpiozero import LED, Button # Import GPIO library: https://gpiozero.readthedocs.io/en/stable/
+from time import sleep
+from losantmqtt import Device # Import Losant library: https://github.com/Losant/losant-mqtt-python
+
+led_gpio = 23
+button_gpio = 21
+
+led = LED(led_gpio)
+button = Button(button_gpio)
+
+# Construct Losant device
+device = Device("my-device-id", "my-app-access-key", "my-app-access-secret")
+
+def on_command(device, command):
+    print(command["name"] + " command received.")
+
+    # Listen for the gpioControl. This name configured in Losant
+    if command["name"] == "toggle":
+        # toggle the LED
+        led.toggle()
+
+def sendDeviceState():
+    device.send_state({"button": True})
+
+# Listen for commands.
+device.add_event_observer("command", on_command)
+
+print("Listening for device commands")
+
+button.when_pressed = sendDeviceState # Send device state when button is pressed
+
+# Connect to Losant and leave the connection open
+device.connect(blocking=True)
